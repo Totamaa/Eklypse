61b8d53cab545b2a3e9fec52ce013f3ba08d4518
Raphaël Guingois <rphl.gngs@gmail.com>
2022-03-05 08:41:08 +0100

    Modification de la sauvegarde. Elle prend désormais en compte les variables en rapport avec le monde (tick, hours, nb_day)

--- script/Game.gd
+++ script/Game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+# Chemin du script pour les fonctions de sauvegarde
 const saving_script = preload("res://script/save.gd")
 
 export var duree_day = 1
@@ -17,19 +18,39 @@ var nb_day = 0
 enum {JOUR, NUIT}
 var cycle = JOUR
 
-
+# Quand on entre dans le jeu (à modifier quand le joueur pourra choisir "nouvelle partie" ou "charger partie"
 func _on_Game_tree_entered():
+	# On load les données sauvegardées
 	var data = saving_script.load_data()
-	get_node("Player").set("position", str2var("Vector2" + data["player"]["position"]))
+	# On récupère le node "Player"
+	var player = get_node("Player")
+	# On parcours les données sauvegardées
+	for keys in data:
+		# Si les données appartiennent au joueur
+		if keys == "player":
+			# Alors on récupère les données liées au joueur (ici uniquement la position)
+			for p_keys in data[keys]:
+				player.set(p_keys, str2var("Vector2" + data[keys][p_keys]))
+		# Si les données appartiennent au monde (heurs, jours ...)
+		elif keys == "world":
+			# Alors on récupère les données liées au monde
+			tick = data[keys]["tick"]		# Les ticks pour connaître le moment exact de la journée
+			hours = data[keys]["hours"]		# L'heure pour avoir un variable plus commune que les ticks
+			nb_day = data[keys]["nb_day"]	# Le nombre de jour(s) passé(s)
 
 # Fonction pour appeler la sauvegarde quand le jeu se ferme
 func _on_Game_tree_exited():
-	saving_script.save_on_quit(get_node("Player").get_property())
+	# On récupère les données concernant le monde
+	var world_data = {
+		"tick": tick,
+		"hours": hours, 
+		"nb_day": nb_day
+		}
+	# On appelle la fonction qui permet de sauvegarder les données
+	saving_script.save_on_quit(world_data, get_node("Player").get_property())
 
 func _ready():
 	length_day = 60 * 60 * duree_day
-	
-	tick = length_day / 2
 	$Player/Light2D.hide()
 
 func _physics_process(delta):
--- script/save.gd
+++ script/save.gd
@@ -1,29 +1,54 @@
+# Les données à sauvegarder au format JSON
 const data = {
 	"player":	{
 		"position": null,
 	},
-	"ennemie":	{
-		"name": "test",
-	}
+	"world":	{
+		"tick": 0,
+		"hours": 0,
+		"nb_day": 0,
+	},
 }
 
+# Chemin du fichier de sauvegarde (dans le répertoire du jeu pour le rendre plus accessible mais à changer à la fin)
 const save_path = "res://save.json"
 
-
-static func save_on_quit(p_data):
+# Fonction qui permet de sauvegarder lorsque le joueur quitte le jeu
+static func save_on_quit(world_data, player_data):
+	# Création d'un objet "File"
 	var file = File.new()
+	# On affecte file à un fichier en mode écriture
 	var isOpen = file.open(save_path, File.WRITE)
+	# Si tout est ok
 	if isOpen == OK:
-		data["player"]["position"] = p_data["position"]
-		data["ennemie"]["name"] = "Gourk"
+		# On parcours les données concernant le monde
+		for w_keys in world_data:
+			# On les stocks dans la variable "data"
+			if w_keys in data["world"]:
+				data["world"][w_keys] = world_data[w_keys]
+		# On parcours les données concernant le joueur
+		for p_keys in player_data:
+			# On les stocks dans la variable "data"
+			if p_keys in data["player"]:
+				data["player"][p_keys] = player_data[p_keys]
+		# On écrit la variable "data" en JSON dans le fichier
 		file.store_line(to_json(data))
+		# On ferme le fichier
 		file.close()
 
+# Fonction qui permet de charger les données
 static func load_data():
+	# Création d'un objet "File"
 	var file = File.new()
+	# Création de la variable qui contiendra les données
 	var loaded_data
+	# On vérifie que le fichier de sauvegarde existe
 	if file.file_exists(save_path):
+		# Si oui on l'ouvre en mode lecture
 		file.open(save_path,File.READ)
+		# On récupère les données
 		loaded_data = parse_json(file.get_line())
+		# On ferme le fichier
 		file.close()
+		# Puis on retourne les données
 		return loaded_data
